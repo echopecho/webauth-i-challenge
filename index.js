bae1b92bdffb0fa1074403491a66896a28e6cd18
echopecho <pbsmithtbn@gmail.com>
2019-03-27 21:37:10 -0500

    Add basic authentication

--- index.js
+++ index.js
@@ -16,12 +16,63 @@ server.post('/api/register', async (req, res) => {
 
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
+  try {
+    const newUser = await Users.add(user);
+    res.status(201).json(newUser);
+  } catch (e) {
+    res.status(500).json({error: "Something went wrong with the server."})
+  }
+  
+});
 
-  const newUser = await Users.add(user);
-  res.status(201).json(newUser);
+server.post('/api/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await Users.findBy({ username });
+
+    if(user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({message: `Welcome ${username}`});
+    } else {
+      res.status(401).json({error: "Invalid Credentials."})
+    }
+  } catch (e) {
+    res.status(500).json({error: "Something went wrong with the server."});
+  }
+});
+
+async function restricted(req, res, next) {
+  const { username, password } = req.headers;
+
+  if(username && password) {
+    try {
+      const user = await Users.findBy({username});
+
+      if(user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({message: "Invalid Credentials."})
+      }
+    } catch (e) {
+      res.status(500).json({error: "Something went wrong with the server."})
+    }
+  } else {
+    res.status(500).json({message: "Missing Credentials"});
+  }
+}
+
+server.get('/api/users', restricted, async (req, res) => {
+
+  try {
+    const users = await Users.find();
+    res.status(200).json(users)
+  } catch (e) {
+    res.status(500).json({error: "Something went wrong with the server."})
+  }
+  
 })
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
-})
+});
--- users/user-model.js
+++ users/user-model.js
@@ -5,6 +5,14 @@ module.exports = {
   add: async (user) => {
     const [id] = await db('users').insert(user);
 
-    return db('users').where('id', id).first();
+    return db('users').where(id).first();
+  },
+
+  find: () => {
+    return db('users');
+  },
+
+  findBy: (user) => {
+    return db('users').where(user).first();
   }
 }
